e2316a65265432c00c9adc5f580020bb76a76a44
Amith Sreekumar <amithsreekumar@Amiths-MacBook-Air.local>
2026-03-29 00:28:34 +0530

    deployment fix

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 keyword_hints={
     "subarray":"Try sliding window or prefix sum approach",
@@ -137,5 +138,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
